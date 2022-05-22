850e2b55903628c18aa1b0ed6cf7340558938d52
Sardorbek3101 <oscodeer@gmail.com>
2022-05-22 22:53:23 +0500

    python

--- python.py
+++ python.py
@@ -450,3 +450,125 @@
 # toys = {'ball' , 'car' , 'lamp' , 'ball' , 'bear' , 'car'} #type - set qaytarilgan qiymatni bittasini korsatadi
 # print(toys)
 # =============================================================================
+
+#NESTING
+# =============================================================================
+# car0 = {
+#         'model' : 'lacetti',
+#         'rang' : 'oq',
+#         'yil' : 2018,
+#         'narh' : 13000,
+#         'km' : 50000,
+#         'korobka' : 'avtomat'
+#         }
+# car1 = {
+#         'model' : 'nexia 3',
+#         'rang' : 'qora',
+#         'yil' : 2015,
+#         'narh' : 9000,
+#         'km' : 89000,
+#         'korobka' : 'mexanika',
+#         }
+# car2 = {
+#         'model' : 'gentra', 
+#         'rang' : 'qizil',
+#         'yil' : 2015,
+#         'narh' : 9000,
+#         'km' : 89000,
+#         'korobka' : 20000,
+#         }
+# 
+# cars = [car0 , car1 , car2]
+# for car in cars:
+#     print(f"{car['model'].title()} , "
+#           f"{car['rang']} rang , "
+#           f"{car['yil']}-yil , {car['narh']}$ "
+#           )
+# print(cars[0]['model'])
+# print(cars[2]['rang'] , cars[2]['model'])
+# =============================================================================
+
+# =============================================================================
+# malibus = []
+# for n in range(10):
+#     new_car = {
+#             'model' : 'malibu', 
+#             'rang' : None,
+#             'yil' : 2022,
+#             'narh' : None,
+#             'km' : 0,
+#             'korobka' : 'avto',
+#             }
+#     malibus.append(new_car)
+# 
+# #for malibu in malibus:
+# #    print(malibu)
+# 
+# for malibu in malibus[:3]:
+#     malibu['rang'] = 'qizil'
+#     
+# for malibu in malibus[3:6]:
+#     malibu['rang'] = 'qora'
+#     
+# for malibu in malibus[6:]:
+#     malibu['rang'] = 'qora'
+#     malibu['korobka'] = 'mexanika'
+#     
+# for malibu in malibus:
+#     if malibu['korobka'] == 'avto':
+#         malibu['narh'] = 40000
+#     else:
+#         malibu['narh'] = 35000
+#         
+# for malibu in malibus :
+#     print(malibu)
+# =============================================================================
+
+# =============================================================================
+# dasturchilar = {
+#     'ali' : ['python' , 'c++'],
+#     'vali' : ['html' , 'css' , 'js'],
+#     'hasan' : ['php' , 'sql'],
+#     'husan' : ['python' , 'php'],
+#     'maryam' : ['c++' , 'c#'],
+#     }
+# # =============================================================================
+# # for ism , tillar in dasturchilar.items():
+# #     print(f"\n{ism.title()} quyidgi dasturlash tilini biladi:")
+# #     for til in tillar:
+# #         print(til.upper())
+# # =============================================================================
+#         
+# for ism , tillar in dasturchilar.items():
+#     print(f"\n\n{ism.title()} quyidgi dasturlash tilini biladi:")
+#     for til in tillar:
+#         print(til.upper() , end = ' ')
+# =============================================================================
+
+# =============================================================================
+# hamkasblar = {
+#     'ali' : {'familiya' : 'valiyev',
+#              'tyil' : 1995,
+#              'malumot' : 'oliy',
+#              'tillar' : ['python' , 'c++']
+#         } ,
+#     'vali' : {'familiya' : 'aliyev',
+#              'tyil' : 2001,
+#              'malumot' : "o'rta-maxsus",
+#              'tillar' : ['html' , 'css' , 'js']
+#         },
+#     'hasan' : {'familiya' : 'husanov',
+#              'tyil' : 1999,
+#              'malumot' : 'maxsus',
+#              'tillar' : ['python' , 'php']
+#         } 
+#     }
+# for ism , info in hamkasblar.items():
+#     print(f"\n{ism.title()} {info['familiya'].title()} ,\n"
+#           f"{info['tyil']}-yil da tug'ilgan , "
+#           f"malumoti {info['malumot']}.\n"
+#           f"Quyidagi dasturlash tillarini biiladi : ")
+#     for til in info['tillar']:
+#         print(til.upper())
+# 
+# =============================================================================
